150f141b95ee24d67bfc97b582a60e08ebda4898
hs-glenn-lee <git_test@mail.com>
2018-11-06 20:06:52 +0900

    SinglyLinkedList

--- src/hsGlennLee/linkedList/SinglyLinkedList.java
+++ src/hsGlennLee/linkedList/SinglyLinkedList.java
@@ -1,5 +1,84 @@
 package hsGlennLee.linkedList;
 
-public class SinglyLinkedList {
+/* Reference Data Structures and Algorithms in Java 6th Edition */
+/*
+ * E = Element 엘리먼트
+ * 접근 메소드는 Element를 반환한다.
+ * */
+public class SinglyLinkedList<E> {
 
+	private static class Node<E> {
+		private E element;
+		private Node<E> next;
+
+		public Node(E e, Node<E> n) {
+			element = e;
+			next = n;
+		}
+
+		public E getElement() {
+			return element;
+		}
+
+		public Node<E> getNext() {
+			return next;
+		}
+
+		public void setNext(Node<E> n) {
+			next = n;
+		}
+	}
+
+	private Node<E> head = null;
+	private Node<E> tail = null;
+	private int size = 0;
+
+	public SinglyLinkedList() {	}
+	
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public E first() {
+		if (isEmpty())
+			return null;
+		return head.getElement();
+	}
+
+	public E last() {
+		if (isEmpty())
+			return null;
+		return tail.getElement();
+	}
+
+	public void addFirst(E e) {
+		head = new Node<>(e, head);
+		if (size == 0)
+			tail = head;
+		size++;
+	}
+
+	public void addLast(E e) {
+		Node<E> newest = new Node<>(e, null);
+		if (isEmpty())
+			head = newest;
+		else
+			tail.setNext(newest);
+		tail = newest;
+		size++;
+	}
+
+	public E removeFirst( ) {
+		if (isEmpty( )) return null;
+		E answer = head.getElement();
+		head = head.getNext();
+		size--;
+		if (size == 0)
+			tail = null;
+		return answer;
+	}
 }
